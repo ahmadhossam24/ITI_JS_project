8fdb49aca8f999e7597a6ba7e02d8ea478486f88
ahmadhossam24 <70440432+ahmadhossam24@users.noreply.github.com>
2023-09-25 20:06:39 +0300

    for eng.basma

---
+++ Project.js
@@ -0,0 +1,11 @@
+// eng.Ahmed code
+if (document.title=="Gold shop"){
+    document.getElementsByTagName("body")[0].style.height="300%"
+}
+
+// eng.Mohamed code
+
+// eng.Sarah code
+
+// eng.Basma code
+
